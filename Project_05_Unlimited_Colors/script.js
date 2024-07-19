3268ce2c382c3b7ffbbfe70fe2b613a1d20a455b
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-07-19 21:12:09 +0530

    Project-05 Umlimited Color

---
+++ Project_05_Unlimited_Colors/script.js
@@ -0,0 +1,72 @@
+// generate a random color
+
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
+  let intervalId;
+
+  const startChangingColor = function () {
+    //  Corner cases
+    if (!intervalId) {
+      intervalId = setInterval(changeBgColor, 1000);
+    }
+  
+    function changeBgColor() {
+      document.body.style.backgroundColor = randomColor();
+    }
+  };
+
+  const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+  };
+  
+  document.querySelector('#start').addEventListener('click', startChangingColor);
+  
+  document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
+
+
+  
+
+// generate a random color
+
+// const randomColor = function() {
+//     const hex = "0123456789ABCDEF"
+//     let color = '#'
+//     for(let i = 0; i < 6; i++) {
+//         color += hex[Math.floor(Math.random() * 16)]
+//     }
+//     return color;
+// };
+
+// // console.log(Math.random());
+// console.log(randomColor());
+
+
+// let intervalId
+// const startChangingColor = function() {
+
+//     intervalId = setInterval(changeBgColor, 1000);
+
+//     function changeBgColor(){
+//         document.body.style.backgroundColor = randomColor();
+//     }
+// };
+
+
+// const stopChangingColor = function() {
+
+//     clearInterval(intervalId);
+//     intervalId = null;
+// };
+
+// document.querySelector('#start').addEventListener('click', startChangingColor) 
+
+// document.querySelector('#stop').addEventListener('click', stopChangingColor) 
